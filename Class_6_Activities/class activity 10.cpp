b9c5412f12d37a8aa874d219d0e24955a8578698
John Gill <144708554+jagill12@users.noreply.github.com>
2025-04-09 16:27:22 -0400

    Add files via upload

---
+++ Class_6_Activities/class activity 10.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+void bucketsort(int a[], int n){
+	int max = a[0];
+	for(int i = 1; i < n; i++)
+		if (a[i] > max)
+			max = a[i];
+	int b[max], i;
+	for (int i = 0; i <= max; i++) {
+		b[i] = 0;
+	}
+	for (int i = 0; i < n; i++) {
+		b[a[i]]++;
+	}
+	for (int i = 0, j = 0; i <= max; i++) {
+		while (b[i] > 0) {
+			a[j++] = i;
+			b[i]--;
+		}
+	}
+}
+int main(){
+	int a[] = {12, 45, 33, 87, 56, 9, 11, 7, 67};
+	int n = sizeof(a) / sizeof(a[0]);
+	printf("Before sorting array elements are: \n");
+	for (int i = 0; i < n; i++)
+		printf("%d ", a[i]);
+		bucketsort(a, n);
+	printf("\nAfter sorting array elements are: \n");
+	for (int i = 0; i < n; i++)
+		printf("%d ", a[i]);
+}
